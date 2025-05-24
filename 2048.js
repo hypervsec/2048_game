b52ebea9e4fbfeb386c49483a8a555918528e086
Enes <91144980+hypervsec@users.noreply.github.com>
2025-05-24 16:25:47 +0300

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -380,10 +380,3 @@ window.addEventListener('resize', () => {
   document.body.style.height = window.innerHeight + 'px';
 });
 
-
-const setVH = () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
-  window.addEventListener('resize', setVH);
-  window.addEventListener('load', setVH);
